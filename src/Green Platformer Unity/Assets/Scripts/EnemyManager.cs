a47425d73c216bef2bcd234ccb655dba35368f49
Benjamin <benjamin.hate@enseirb-matmeca.fr>
2019-08-27 23:43:07 +0200

    Added EnemyManager and Player can damage enemy

---
+++ src/Green Platformer Unity/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityExtensions.ScriptableTools.Variables;
+
+public class EnemyManager : MonoBehaviour
+{
+    public FloatReference initialHealth;
+    public float health;
+
+    public bool IsDead => health <= 0;
+    
+    private void Awake()
+    {
+        health = initialHealth.Value;
+    }
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+
+        if (IsDead)
+            Death();
+    }
+
+    private void Death()
+    {
+        Destroy(gameObject);
+    }
+}
